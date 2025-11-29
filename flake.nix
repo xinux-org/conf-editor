4b8ad87478e39f77bf6c05ba31a5fd5759cdaf9c
bahrom04 <magdiyevbahrom@gmail.com>
2025-11-29 13:02:16 +0500

    chore: move to 25.11

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "github:xinux-org/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:xinux-org/nixpkgs/nixos-25.11";
     xinux-lib = {
-      url = "github:xinux-org/lib";
+      url = "github:xinux-org/lib/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
